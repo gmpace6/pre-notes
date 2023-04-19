f0b713981bd4c14680bd8cb1e64b2d13ed193cbc
Garrett Pace <elderpace6@gmail.com>
2023-04-19 11:03:48 -0600

    better!

--- pre-notes.js
+++ pre-notes.js
@@ -1,87 +1,90 @@
-let age = 21
+// let age = 21
 
-function logDetails() {
-  let name = 'Tyler'
-  console.log(`My name is ${name} and I am ${age}.`)
-}
-// console.log(logDetails())
+// function logDetails() {
+//   let name = 'Tyler'
+//   console.log(`My name is ${name} and I am ${age}.`)
+// }
+// // console.log(logDetails())
 
-const dog = {
-    'name': 'Freida',
-    'color': 'brown/black',
-    'hunger': 40,
-    'mood': 'feisty',
-    'age': 9,
-  };
-//   console.log(dog)
+// const dog = {
+//     'name': 'Freida',
+//     'color': 'brown/black',
+//     'hunger': 40,
+//     'mood': 'feisty',
+//     'age': 9,
+//   };
+// //   console.log(dog)
 
-  function giveMeFive() {
-    return 5
-}
-// console.log(giveMeFive())
-
-
-const sayHi = function() {
-    return 'Hi!'
-}
-// console.log(sayHi())
+//   function giveMeFive() {
+//     return 5
+// }
+// // console.log(giveMeFive())
 
 
-const sayBye = () => {
-    return 'Bye!'
-}
-// console.log(sayBye())
+// const sayHi = function() {
+//     return 'Hi!'
+// }
+// // console.log(sayHi())
 
 
-const returnParam = item => {
-    return item
-}
-// console.log(returnParam())
+// const sayBye = () => {
+//     return 'Bye!'
+// }
+// // console.log(sayBye())
 
 
-const makeArr = (one, two, three) => {
-    let arr = [one, two, three]
-    return arr
-}
-// console.log(makeArr())
+// const returnParam = item => {
+//     return item
+// }
+// // console.log(returnParam())
 
 
-const giveMeFive1 = () => 5
-// console.log(giveMeFive1())
+// const makeArr = (one, two, three) => {
+//     let arr = [one, two, three]
+//     return arr
+// }
+// // console.log(makeArr())
 
 
-const addFive = num => num + 5
-// console.log(addFive())
+// const giveMeFive1 = () => 5
+// // console.log(giveMeFive1())
 
 
-const makePriceObj = number => ({price: number})
-// console.log(makePriceObj)
+// const addFive = num => num + 5
+// // console.log(addFive())
 
 
-const makeLargeObj = (str, num, arr) => (
-    {
-        word: "str",
-        integer: "num",
-        list: "arr",
-    }
-)
-// console.log(makeLargeObj)
+// const makePriceObj = number => ({price: number})
+// // console.log(makePriceObj)
 
 
-const createAdder = x => (y) => x + y
+// const makeLargeObj = (str, num, arr) => (
+//     {
+//         word: "str",
+//         integer: "num",
+//         list: "arr",
+//     }
+// )
+// // console.log(makeLargeObj)
 
-// here's the same function written as a declaration
-// function createAdder(x) {
-//    return function(y) {
-//        return x + y
-//    }
-//}
 
-const addFive1 = createAdder(5)
-const addTen = createAdder(10)
+// const createAdder = x => (y) => x + y
 
-addFive1(12) // output: 17
-addFive(40) // output: 45
-addTen(3) // output: 13
+// // here's the same function written as a declaration
+// // function createAdder(x) {
+// //    return function(y) {
+// //        return x + y
+// //    }
+// //}
 
-console.log(createAdder())
+// const addFive1 = createAdder(5)
+// const addTen = createAdder(10)
+
+// addFive1(12) // output: 17
+// addFive(40) // output: 45
+// addTen(3) // output: 13
+
+// console.log(createAdder())
+
+const addFive = (num) => num + 5
+console.log(addFive(1))
