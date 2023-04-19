48ad6da921125917848e635b02ed93b034b754c0
Garrett Pace <elderpace6@gmail.com>
2023-04-19 09:27:32 -0600

    done?

--- pre-notes.js
+++ pre-notes.js
@@ -4,7 +4,7 @@ function logDetails() {
   let name = 'Tyler'
   console.log(`My name is ${name} and I am ${age}.`)
 }
-console.log(logDetails())
+// console.log(logDetails())
 
 const dog = {
     'name': 'Freida',
@@ -13,22 +13,75 @@ const dog = {
     'mood': 'feisty',
     'age': 9,
   };
-  console.log(dog)
+//   console.log(dog)
 
   function giveMeFive() {
     return 5
 }
-console.log(giveMeFive())
+// console.log(giveMeFive())
 
 
 const sayHi = function() {
     return 'Hi!'
 }
-console.log(sayHi())
+// console.log(sayHi())
 
 
 const sayBye = () => {
     return 'Bye!'
 }
-console.log(sayBye())
+// console.log(sayBye())
 
+
+const returnParam = item => {
+    return item
+}
+// console.log(returnParam())
+
+
+const makeArr = (one, two, three) => {
+    let arr = [one, two, three]
+    return arr
+}
+// console.log(makeArr())
+
+
+const giveMeFive1 = () => 5
+// console.log(giveMeFive1())
+
+
+const addFive = num => num + 5
+// console.log(addFive())
+
+
+const makePriceObj = number => ({price: number})
+// console.log(makePriceObj)
+
+
+const makeLargeObj = (str, num, arr) => (
+    {
+        word: "str",
+        integer: "num",
+        list: "arr",
+    }
+)
+// console.log(makeLargeObj)
+
+
+const createAdder = x => (y) => x + y
+
+// here's the same function written as a declaration
+// function createAdder(x) {
+//    return function(y) {
+//        return x + y
+//    }
+//}
+
+const addFive1 = createAdder(5)
+const addTen = createAdder(10)
+
+addFive1(12) // output: 17
+addFive(40) // output: 45
+addTen(3) // output: 13
+
+console.log(createAdder())
