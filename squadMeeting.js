6161d0ecda0606c27ce71538b349f569c4b38d2b
Garrett Pace <elderpace6@gmail.com>
2023-04-19 10:19:25 -0600

    done

---
+++ squadMeeting.js
@@ -0,0 +1,26 @@
+// let x = "5"
+// let y = "8"
+
+
+// function largest(x, y){
+//      if(x > y)
+//     {
+//         console.log(x)
+//         }else{
+//     console.log(y)
+//     } 
+// }
+
+// largest (x, y)
+
+
+// let x = "5"
+// let y = "8"
+
+
+// function add(x, y){
+//      return x + y
+// }
+
+// console.log(add(x, y))
+
