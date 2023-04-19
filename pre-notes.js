dbdb571b921366d323767870a5eaddda5b568dc8
Garrett Pace <elderpace6@gmail.com>
2023-04-19 09:07:24 -0600

    1st

---
+++ pre-notes.js
@@ -0,0 +1,34 @@
+let age = 21
+
+function logDetails() {
+  let name = 'Tyler'
+  console.log(`My name is ${name} and I am ${age}.`)
+}
+console.log(logDetails())
+
+const dog = {
+    'name': 'Freida',
+    'color': 'brown/black',
+    'hunger': 40,
+    'mood': 'feisty',
+    'age': 9,
+  };
+  console.log(dog)
+
+  function giveMeFive() {
+    return 5
+}
+console.log(giveMeFive())
+
+
+const sayHi = function() {
+    return 'Hi!'
+}
+console.log(sayHi())
+
+
+const sayBye = () => {
+    return 'Bye!'
+}
+console.log(sayBye())
+
